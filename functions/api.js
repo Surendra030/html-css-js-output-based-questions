41eac097516dcbc60aa350fdd0d76d95330ce550
Surendra030 <surendrababun030@gmail.com>
2024-09-07 02:07:10 +0530

    adding api integration setup files

---
+++ functions/api.js
@@ -0,0 +1,23 @@
+// functions/api.js
+const code_base_questions = require('./output_based.js'); // Import the questions
+
+exports.handler = async function (event, context) {
+  if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(code_base_questions) // Return the questions as JSON
+    };
+  } else if (event.httpMethod === 'POST') {
+    const body = JSON.parse(event.body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `You sent: ${body.name}` })
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: "Method Not Allowed" })
+    };
+  }
+};
